9c86340a89a9b72b93a147410a0200f9cebaf2e5
Liz <erbanach@gmail.com>
2019-02-25 14:51:45 -0800

    routes for home + cart

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
 
 const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/cart" component={Cart} />
+    </div>
+  </Router>
+)
+
+const Home = () => (
   <div>
-    <h2>Shopping Cart Example</h2>
-    <hr/>
     <ProductsContainer />
-    <hr/>
+  </div>
+)
+
+const Cart = () => (
+  <div>
     <CartContainer />
   </div>
 )
