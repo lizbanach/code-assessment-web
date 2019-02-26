1b6a837f84d52e36f5707d28451a97dee95ad866
Liz <erbanach@gmail.com>
2019-02-25 21:24:11 -0800

    add some divs to product in product list, make a new product item for the cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Product = ({ price, inventory, title }) => (
+const CartItem = ({ price, inventory, title }) => (
   <div>
     <div>{title} - &#36;{price}</div>
-    <div>{inventory} remaining</div>
   </div>
 )
 
-Product.propTypes = {
+CartItem.propTypes = {
   price: PropTypes.number,
   inventory: PropTypes.number,
   title: PropTypes.string
 }
 
-export default Product
+export default CartItem
