a8814cd280e3addf5621167a6b555dfa15d319cc
Liz <erbanach@gmail.com>
2019-02-28 00:45:56 -0800

    add svg support, clean up scss framework, start styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import SVG from 'react-inlinesvg';
 
 const Header = ({ onOpenCartClicked, title }) => (
-  <div>
-    <div>{title}</div>
-    <button
-      onClick={onOpenCartClicked}>
-      cart
-    </button>
+  <div className="navigation">
+    <div className="row">
+      <div className="col col-is-10">
+        <h1 className="txt-bold">{title}</h1>
+      </div>
+      <div className="col col-is-2">
+        <button
+          onClick={onOpenCartClicked}>
+          <SVG src="svg/cart.svg"/>
+          Cart
+        </button>
+      </div>
+    </div>
   </div>
 )
 
