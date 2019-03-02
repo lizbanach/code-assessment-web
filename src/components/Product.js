2bcbab8ffd2538e38e9383bf6ea3e3ac854fe46a
Liz <erbanach@gmail.com>
2019-03-01 17:03:15 -0800

    fix structure of product in product list

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Product = ({ price, inventory, title }) => {
+const Product = ({ price, inventory, title, onAddToCartClicked }) => {
   const imageTitle = title.toLowerCase()
   const image = '/img/image-' + imageTitle + '.jpg'
   return (
     <div className="row">
-      <div className="col lg-col-is-flush lg-col-is-4 col-is-12 txt-center">
+      <div className="col lg-col-is-4 col-is-12 txt-center">
         <img className="product-image" alt={title} src={image} />
       </div>
-      <div className="col lg-col-is-flush col-is-6 lg-col-is-4">
+      <div className="col col-is-6 lg-col-is-4">
+      <div className="lead-1 lead-bottom-1">
         <h2>{title}</h2>
         <p className="txt-uppercase txt-gray txt-light txt-size-1 lead-half">{inventory} remaining</p>
+        <button
+          className="btn btn-blue txt-uppercase txt-size-1 ns-lead-2 lead-1"
+          onClick={onAddToCartClicked}
+          disabled={inventory > 0 ? '' : 'disabled'}>
+          {inventory > 0 ? 'Add to cart' : 'Sold Out'}
+        </button>
+        </div>
       </div>
-      <div className="col lg-col-is-flush lg-col-is-4 col-is-6 txt-right">
-        <p>&#36;{price}</p>
+      <div className="col lg-col-is-4 col-is-6 txt-right">
+        <div className="lead-1">
+          <p>&#36;{price}</p>
+        </div>
       </div>
     </div>
   )
@@ -24,7 +34,8 @@ Product.propTypes = {
   price: PropTypes.number,
   inventory: PropTypes.number,
   title: PropTypes.string,
-  image: PropTypes.string
+  image: PropTypes.string,
+  onAddToCartClicked: PropTypes.func.isRequired
 }
 
 export default Product
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Product from './Product'
+import { addToCart } from '../actions'
+import { connect } from 'react-redux'
 
-const ProductItem = ({ product, onAddToCartClicked }) => {
+const ProductItem = ({ product, addToCart }) => {
   return (
-  <div className="product col col-is-12 lead-1">
+  <div className="product col col-is-flush col-is-12 lead-1">
     <Product
       title={product.productTitle}
       price={product.price.value}
-      inventory={product.inventory} />
-      <div className="col lg-col-is-flush lg-is-offset-4 lead-1 lg-lead-0">
-        <button
-          className="btn btn-blue txt-uppercase txt-size-1"
-          onClick={onAddToCartClicked}
-          disabled={product.inventory > 0 ? '' : 'disabled'}>
-          {product.inventory > 0 ? 'Add to cart' : 'Sold Out'}
-        </button>
-    </div>
+      inventory={product.inventory}
+      onAddToCartClicked={() => addToCart(product.id)} />
   </div>
   )
 }
@@ -29,7 +24,14 @@ ProductItem.propTypes = {
       value: PropTypes.number.isRequired,
     })
   }).isRequired,
-  onAddToCartClicked: PropTypes.func.isRequired
+  addToCart: PropTypes.func.isRequired
 }
 
-export default ProductItem
+const mapStateToProps = state => ({
+  cart: state.cart
+})
+
+export default connect(
+  mapStateToProps,
+  { addToCart }
+)(ProductItem)
